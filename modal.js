0931a82f8667d68bae526c2c2defd00b94527b89
5mitty <43888178+5mitty@users.noreply.github.com>
2024-09-27 11:13:06 -0500

    files up to date

--- modal.js
+++ modal.js
@@ -1,30 +1,30 @@
-    const message = document.getElementById('message-text');
-    const email = document.getElementById('recipient-name');
-    const submitButton = document.getElementById('submitButton');
-    const popMessageDiv = document.getElementById('popMessage');
+const message = document.getElementById('message-text');
+const email = document.getElementById('recipient-name');
+const submitButton = document.getElementById('submitButton');
+const popMessageDiv = document.getElementById('popMessage');
 
 function displayMessage(type, submitMessage) {
-    popMessageDiv.textContent = submitMessage;
-    popMessageDiv.setAttribute('class', type);
+popMessageDiv.textContent = submitMessage;
+popMessageDiv.setAttribute('class', type);
 }
 
-    submitButton.addEventListener('click', function (event) {
+submitButton.addEventListener('click', function (event) {
 
-        event.preventDefault();
-        const userInput = {
-            email: email.value.trim(),
-            message: message.value.trim(),
-        };
-        if (email.value !== '' && message.value !== '') {
-            displayMessage('success', 'Message sent successfully!');
-        } else {
-            displayMessage('error', 'Input field(s) cannot be blank.');
-        }
-        localStorage.setItem('userInput', JSON.stringify(userInput));   
-    });
+    event.preventDefault();
+    const userInput = {
+        email: email.value.trim(),
+        message: message.value.trim(),
+    };
+    if (email.value !== '' && message.value !== '') {
+        displayMessage('success', 'Message sent successfully!');
+    } else {
+        displayMessage('error', 'Input field(s) cannot be blank.');
+    }
+    localStorage.setItem('userInput', JSON.stringify(userInput));   
+});
 
 
 function resetForm() {
-    document.getElementById('contactUsForm').reset();
-    document.getElementById('popMessage').remove();
+document.getElementById('contactUsForm').reset();
+document.getElementById('popMessage').remove();
 };
